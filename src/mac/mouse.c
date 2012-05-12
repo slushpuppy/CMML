6c4f672d2abdafe8d114ad7b4b4b850b11917cab
John Peel <Dgby714@gmail.com>
2012-05-12 07:03:21 -0400

    Mac Stubs

---
+++ src/mac/mouse.c
@@ -0,0 +1,28 @@
+#include "mouse.h"
+
+#include <string.h>
+
+void mouseMove(uint32_t x, uint32_t y)
+{
+
+}
+
+void mousePos(uint32_t *x, uint32_t *y)
+{
+
+}
+
+static DWORD _b2b(mouseButton b, int up)
+{
+
+}
+
+void mouseDown(mouseButton button)
+{
+	
+}
+
+void mouseUp(mouseButton button)
+{
+	
+}
