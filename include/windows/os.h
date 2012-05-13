4c9295ce4b4ac5bf8aa8ae878d38c00c58ee9081
John Peel <Dgby714@gmail.com>
2012-05-12 20:08:49 -0400

    Fix windows compilation.

--- include/windows/os.h
+++ include/windows/os.h
@@ -3,7 +3,14 @@
 
 #include "mml.h"
 
+#ifndef _WIN32_WINNT
+#ifndef _WIN32_WINNT_WINXP
+#define _WIN32_WINNT_WINXP 0x0501
+#endif
+
 #define _WIN32_WINNT _WIN32_WINNT_WINXP
+#endif
+
 #include <windows.h>
 
 #endif
