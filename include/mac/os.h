86a9e5e3feaaf214d245fbdd0fc0174c590b4f5d
John Peel <Dgby714@gmail.com>
2012-05-16 23:08:32 -0400

    Some more mac stuff

--- include/mac/os.h
+++ include/mac/os.h
@@ -1,8 +1,10 @@
 #ifndef __os_h_
 #define __os_h_
 
-#include <Foundation/Foundation.h>
-#include <ApplicationServices/ApplicationServices.h>
+#ifdef __NEED_MAC_H_
+# include <Foundation/Foundation.h>
+# include <ApplicationServices/ApplicationServices.h>
+#endif
 
 typedef int keyboardKey;
 
